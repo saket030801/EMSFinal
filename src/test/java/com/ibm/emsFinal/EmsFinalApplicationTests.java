64a3118366ed18a2e5a3b0878b30398637714e77
saket030801 <saket030801@gmail.com>
2024-03-13 08:50:45 +0530

    First Commit

---
+++ src/test/java/com/ibm/emsFinal/EmsFinalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ibm.emsFinal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmsFinalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
